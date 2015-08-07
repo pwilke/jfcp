726998b3840bdf5421d02bcd14c971d9dfd4595b
Vincent Laporte <vincent.laporte@irisa.fr>
2015-08-08 01:27:54 +0200

    Iterate a function

--- src/jfcp.ml
+++ src/jfcp.ml
@@ -8,3 +8,11 @@ let pp_int fmt = Format.fprintf fmt "%d"
 (* formatting lists *)
 let pp_list hf sep fmt =
   List.iter (Format.fprintf fmt "%s%a" sep hf)
+
+(* iterate a function *)
+let iter (f: 'a -> 'a) (n: int) (a: 'a) : 'a =
+  let rec aux a = function
+  | 0 -> a
+  | n -> aux (f a) (n - 1)
+  in 
+  aux a n
