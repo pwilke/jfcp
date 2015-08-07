9d4c7ba1f7cdf9ef79de3ddaa1ddfe3316c6abdb
Vincent Laporte <vincent.laporte@irisa.fr>
2015-08-08 01:31:19 +0200

    clear warning 40

--- src/simulation.ml
+++ src/simulation.ml
@@ -1,6 +1,7 @@
 open Config
 open Board
 open Orders
+open Pawn
 
 let obind (x: 'a option) (f: 'a -> 'b option) =
   begin match x with
@@ -12,7 +13,7 @@ let doigt : Config.t option -> order list -> Config.t option =
   List.fold_left (fun cfg order ->
     obind cfg (fun cfg ->
     Format.printf "%a\n%s@."
-    (Board.format ~pivot:(Some cfg.Config.p.pivot)) (Config.proj cfg)
+    (Board.format ~pivot:(Some cfg.Config.p.Pawn.pivot)) (Config.proj cfg)
     (string_of_order order)
     ;
     Config.update cfg order
